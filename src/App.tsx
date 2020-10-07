6b1c91f825932bc0f9cdfc5bf210240654e94676
mi yang <7017.mi@gmail.com>
2020-10-07 10:54:00 +0800

    禁用双击放大事件会影响正常点击，采用touch-action属性

--- src/App.tsx
+++ src/App.tsx
@@ -17,28 +17,6 @@ function App() {
     return () => window.removeEventListener('resize', setHight)
   })
 
-  // window.onload = function () {
-  //   // Prevent double click to enlarge
-  //   var lastTouchEnd = 0;
-  //   document.addEventListener('touchstart', function (event) {
-  //     if (event.touches.length > 1) {
-  //       event.preventDefault();
-  //     }
-  //   });
-  //   document.addEventListener('touchend', function (event) {
-  //     var now = (new Date()).getTime();
-  //     if (now - lastTouchEnd <= 300) {
-  //       event.preventDefault();
-  //     }
-  //     lastTouchEnd = now;
-  //   }, false);
-
-  //   // Prevent two fingers from zooming in
-  //   document.addEventListener('gesturestart', function (event) {
-  //     event.preventDefault();
-  //   });
-  // }
-
   return (
     <div className="App">
       <div className="gameboy">
