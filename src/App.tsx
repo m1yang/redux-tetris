7b5679339244f1deb16ce925bf599292c000ee0a
mi yang <7017.mi@gmail.com>
2020-10-07 10:41:30 +0800

    屏蔽mobile zoom

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Scoreboard } from "./features/scoreboard/Scoreboard";
 import { Joystick } from "./features/joystick/Joystick";
 
 function App() {
+  // 兼容mobile高度
   useEffect(()=> {
     const setHight = () => {
       document.documentElement.style
@@ -15,6 +16,28 @@ function App() {
     window.addEventListener('resize', setHight)
     return () => window.removeEventListener('resize', setHight)
   })
+
+  // window.onload = function () {
+  //   // Prevent double click to enlarge
+  //   var lastTouchEnd = 0;
+  //   document.addEventListener('touchstart', function (event) {
+  //     if (event.touches.length > 1) {
+  //       event.preventDefault();
+  //     }
+  //   });
+  //   document.addEventListener('touchend', function (event) {
+  //     var now = (new Date()).getTime();
+  //     if (now - lastTouchEnd <= 300) {
+  //       event.preventDefault();
+  //     }
+  //     lastTouchEnd = now;
+  //   }, false);
+
+  //   // Prevent two fingers from zooming in
+  //   document.addEventListener('gesturestart', function (event) {
+  //     event.preventDefault();
+  //   });
+  // }
 
   return (
     <div className="App">
--- src/features/joystick/Joystick.tsx
+++ src/features/joystick/Joystick.tsx
@@ -175,28 +175,6 @@ export function Joystick() {
     ]
   )
 
-  window.onload = function () {
-    // Prevent double click to enlarge
-    var lastTouchEnd = 0;
-    document.addEventListener('touchstart', function (event) {
-      if (event.touches.length > 1) {
-        event.preventDefault();
-      }
-    });
-    document.addEventListener('touchend', function (event) {
-      var now = (new Date()).getTime();
-      if (now - lastTouchEnd <= 300) {
-        event.preventDefault();
-      }
-      lastTouchEnd = now;
-    }, false);
-
-    // Prevent two fingers from zooming in
-    document.addEventListener('gesturestart', function (event) {
-      event.preventDefault();
-    });
-  }
-
   return (
     // TODO: 摇杆样式
     // 绘制摇杆 用icon会更好看些
